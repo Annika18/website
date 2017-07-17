be6612cf59e7669ef6916b4586cfc80e6437b8a8
Annika18 <klannika@gmail.com>
2017-07-17 15:12:33 -0500

    Add files via upload
    Test i have no idea how this works

---
+++ app.js
@@ -0,0 +1,12 @@
+
+console.log("hello");
+
+console.log("4 + 4 = ", 4+4);
+
+console.log(Math.random());
+
+console.log("This is how to use javascript's command line");
+
+var bands = ["The Beatles", "One Direction", "Nsync", "Maroon 5"];
+
+console.log(bands);
